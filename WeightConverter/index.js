55e147f0854b5e669a2584d0e60cbd476551139c
vanishjangir <79581028+vanishjangir@users.noreply.github.com>
2022-07-20 11:56:29 +0530

    Create index.js

---
+++ WeightConverter/index.js
@@ -0,0 +1,7 @@
+
+
+  const getValueInput = () =>{
+    let inputValue = document.getElementById("inputvalue").value; 
+    document.getElementById("Kg").innerHTML="Your weight in KG: "+inputValue/1000;
+
+  }
